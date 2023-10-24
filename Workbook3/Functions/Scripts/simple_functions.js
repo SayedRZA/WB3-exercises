44e8dc8071f46cc389bf57a1b5eaa46946183cef
SayedRZA <reza.hoss777@gmail.com>
2023-10-24 13:37:35 -0700

    listed my favorite things

---
+++ Workbook3/Functions/Scripts/simple_functions.js
@@ -0,0 +1,27 @@
+listFavoriteThings()
+
+
+function listFavoriteThings() {
+    console.log("My Name");
+    console.log("Inception");
+    console.log("TheWeeknd");
+}
+
+
+whyAmIHere()
+
+function whyAmIHere(){
+
+    console.log("I Am Here To Change the World!!");
+}
+
+
+favoritePlace()
+
+function favoritePlace(){
+
+   console.log("My favorite place is my bed, because sleep has a lot of benefits")
+
+}
+
+console.log("end of file");
